98082707665ff8e252627bc1b7325ed018ffcb4a
INCOM\Aidan.Rypens <aidan.rypens@hotmail.com>
2019-01-18 14:14:15 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const albums = [{
+      'nummer': '6',
+      'titel': 'Het hemelhuis',
+      'kaft': 'Softcover',
+      'prijs': 5.20
+    }, {
+      'nummer': '8',
+      'titel': 'Promo',
+      'kaft': 'Lutgart',
+      'prijs': 6.00
+    }]
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <table>
+          <tr>
+            <th>Nummer</th>
+            <th>Titel</th>
+            <th>Kaft</th>
+            <th>Prijs</th>
+          </tr>
+          {albums.map((album) => {
+            return (
+              <tr>
+                <td>{album.nummer}</td>
+                <td>{album.titel}</td>
+                <td>{album.kaft}</td>
+                <td>{album.prijs}</td>
+              </tr>
+            )
+          })}
+        </table>
       </div>
     );
   }
